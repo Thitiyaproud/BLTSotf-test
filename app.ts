5a528d701a7ff7b88cd76cf41f0b26eed1466467
latte34844 <73719213+latte34844@users.noreply.github.com>
2025-01-02 13:44:20 +0700

    fix: typo

--- app.ts
+++ app.ts
@@ -18,7 +18,7 @@ given:
 
 key features:
 - buy ticket: buy lotto by input number and amount of money and add to customer ticket
-- get ticker: get all ticket that customer buy
+- get ticket: get all ticket that customer buy
 - get random number: select digit that want to buy from 1-6 digits, how many numbers to buy, how much money for each number, and optional customer can select fixed number of digit (example: 5 digits, 10 number, 1000 baht and 4th digit is 5 and 5th digit is 6 then output must be random 10 number that last 2 digit is 56)
 - set draw: set payout randomly one munber and used for the current draw result
 - check win ticket: check winning prize by input all ticket that customer buy and return which number is win and prize
